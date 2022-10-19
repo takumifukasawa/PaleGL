5ffc8832e342a93bba6e137a093e6e39676ad8c9
takumifukasawa <takumifukasawa@gmail.com>
2022-10-19 22:36:28 +0900

    wip transform

--- workspace/perspective-camera/PaleGL/Core/Transform.js
+++ workspace/perspective-camera/PaleGL/Core/Transform.js
@@ -1,8 +1,45 @@
 ﻿import {Matrix4} from "../Math/Matrix4.js";
 
 export class Transform {
-    worldMatrix;
+    parent;
+    #worldMatrix;
+    #localMatrix;
+    #scalingMatrix;
+    #rotationMatrix;
+    #translationMatrix;
+    
+    get worldMatrix() {
+        return this.#worldMatrix;
+    }
+    
+    get localMatrix() {
+        return this.#localMatrix;
+    }
+    
     constructor() {
-        this.worldMatrix = new Matrix4();
+        this.#worldMatrix,
+        this.#localMatrix,
+        this.#scalingMatrix,
+        this.#rotationMatrix,
+        this.#translationMatrix = Matrix4.identity();
+    }
+    
+    update() {
+        this.#worldMatrix = Matrix4.multiplyMatrices(this.parent.worldMatrix, this.localMatrix);
+    }
+
+    // TODO: override matrix
+    scale(s) {
+        this.#scalingMatrix = Matrix4.scaleMatrix(s);
+    }
+
+    // TODO: override matrix
+    rotateZ(rad) {
+        this.#rotationMatrix = Matrix4.rotateZMatrix(rad);
+    }
+
+    // TODO: override matrix
+    translate(v) {
+        this.#translationMatrix = Matrix4.translateMatrix(v);
     }
 }
