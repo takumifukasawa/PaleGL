5e7ce1d1b1c549283ce5ab9d5e72e24edfd522a6
takumifukasawa <takumifukasawa@gmail.com>
2022-10-21 22:31:41 +0900

    draw cube by indices

--- workspace/perspective-camera/script.js
+++ workspace/perspective-camera/script.js
@@ -83,65 +83,43 @@ const boxGeometry = new Geometry({
         position: {
             data: [
                 // front
-                ...boxPosition_0, ...boxPosition_1, ...boxPosition_2,
-                ...boxPosition_2, ...boxPosition_1, ...boxPosition_3,
+                ...boxPosition_0, ...boxPosition_1, ...boxPosition_2, ...boxPosition_3,
                 // right
-                ...boxPosition_2, ...boxPosition_3, ...boxPosition_4,
-                ...boxPosition_4, ...boxPosition_3, ...boxPosition_5,
+                ...boxPosition_2, ...boxPosition_3, ...boxPosition_4, ...boxPosition_5,
                 // back
-                ...boxPosition_4, ...boxPosition_5, ...boxPosition_6,
-                ...boxPosition_6, ...boxPosition_5, ...boxPosition_7,
+                ...boxPosition_4, ...boxPosition_5, ...boxPosition_6, ...boxPosition_7,
                 // left
-                ...boxPosition_6, ...boxPosition_7, ...boxPosition_0,
-                ...boxPosition_0, ...boxPosition_7, ...boxPosition_1,
+                ...boxPosition_6, ...boxPosition_7, ...boxPosition_0, ...boxPosition_1,
                 // top
-                ...boxPosition_6, ...boxPosition_0, ...boxPosition_4,
-                ...boxPosition_4, ...boxPosition_0, ...boxPosition_2,
+                ...boxPosition_6, ...boxPosition_0, ...boxPosition_4, ...boxPosition_2,
                 // bottom
-                ...boxPosition_1, ...boxPosition_7, ...boxPosition_3,
-                ...boxPosition_3, ...boxPosition_7, ...boxPosition_5,
+                ...boxPosition_1, ...boxPosition_7, ...boxPosition_3, ...boxPosition_5,
             ],
             size: 3,
         },
         color: {
             data: [
                 // front: red
-                ...[...new Array(6)].map(i => ([1, 0, 0])).flat(),
+                ...(new Array(4)).fill(0).map(() => ([1, 0, 0])).flat(),
                 // right: blue
-                ...[...new Array(6)].map(i => ([0, 1, 0])).flat(),
+                ...(new Array(4)).fill(0).map(() => ([0, 1, 0])).flat(),
                 // back: green
-                ...[...new Array(6)].map(i => ([0, 0, 1])).flat(),
+                ...(new Array(4)).fill(0).map(() => ([0, 0, 1])).flat(),
                 // left: yellow
-                ...[...new Array(6)].map(i => ([1, 1, 0])).flat(),
+                ...(new Array(4)).fill(0).map(() => ([1, 1, 0])).flat(),
                 // top: purple
-                ...[...new Array(6)].map(i => ([1, 0, 1])).flat(),
+                ...(new Array(4)).fill(0).map(() => ([1, 0, 1])).flat(),
                 // bottom: aqua
-                ...[...new Array(6)].map(i => ([0, 1, 1])).flat(),
+                ...(new Array(4)).fill(0).map(() => ([0, 1, 1])).flat(),
             ],
             size: 3
         },
     },
-    // indices: [
-    //     // front
-    //     0, 1, 2,
-    //     2, 1, 3,
-    //     // right
-    //     2, 3, 4,
-    //     4, 3, 5,
-    //     // back
-    //     4, 5, 6
-    //     6, 5, 7,
-    //     // left
-    //     6, 7, 0,
-    //     0, 7, 1,
-    //     // top
-    //     6, 0, 4,
-    //     4, 0, 2,
-    //     // bottom
-    //     1, 7, 3,
-    //     3, 7, 5
-    // ],
-    drawCount: 3 * 2 * 6 // vertex count
+    indices: Array.from(Array(6).keys()).map(i => ([
+        i * 4 + 0, i * 4 + 1, i * 4 + 2,
+        i * 4 + 2, i * 4 + 1, i * 4 + 3,
+    ])).flat(),
+    drawCount: 6 * 6 // indices count
 });
 
 const geometry = new Geometry({
@@ -206,9 +184,8 @@ const tick = (time) => {
     // rootActor.transform.setRotateZ(time / 1000 * 20);
 
     // mesh.transform.setScale(new Vector3(1, 0.5, 0.5));
-    // mesh.transform.setRotateX(time / 1000 * 30);
+    mesh.transform.setRotateX(time / 1000 * 30);
     // mesh.transform.setRotateY(-time / 1000 * 30);
-    mesh.transform.setRotateY(45);
     // mesh.transform.setRotateZ(time / 1000 * -30);
     // mesh.transform.setTranslate(new Vector3(2, 0, 0));
 
