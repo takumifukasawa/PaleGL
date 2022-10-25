40d8f3ea9efc64dcf125a0565e703c7369ca3f13
takumifukasawa <takumifukasawa@gmail.com>
2022-10-25 23:32:09 +0900

    orghographic camera

---
+++ workspace/orthographic-camera/PaleGL/core/IndexBufferObject.js
@@ -0,0 +1,20 @@
+﻿import {GLObject} from "./GLObject.js";
+
+export class IndexBufferObject extends GLObject {
+    #ibo;
+    
+    get glObject() {
+        return this.#ibo;
+    }
+    
+    constructor({ gpu, indices }) {
+        super();
+        
+        const gl = gpu.gl;
+        
+        this.#ibo = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.#ibo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    }
+}
