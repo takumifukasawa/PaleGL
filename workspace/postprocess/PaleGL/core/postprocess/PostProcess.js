b2b1af1b71f7aafaf0a07d746ae47bf350a7c27a
takumifukasawa <takumifukasawa@gmail.com>
2022-10-28 00:12:00 +0900

    dir for postprocess

---
+++ workspace/postprocess/PaleGL/core/postprocess/PostProcess.js
@@ -0,0 +1,20 @@
+﻿import {OrthographicCamera} from "./../OrthographicCamera.js";
+import {Scene} from "../Scene.js";
+
+export class PostProcess {
+    #orthographicCamera;
+    #scene;
+    #passes;
+    
+    constructor({ gpu }) {
+        this.#orthographicCamera = new OrthographicCamera(-1, 1, -1, 1, -1, 1);
+        this.#scene = new Scene();
+        this.#passes = [];
+    }
+    
+    addPass() {
+    }
+  
+    render() {
+    }
+}
---
+++ workspace/postprocess/PaleGL/core/postprocess/PostProcessPass.js
@@ -0,0 +1,12 @@
+﻿// import {PlaneGeometry} from "../geometries/PlaneGeometry.js";
+// import {Material} from "../Material.js";
+// 
+// export class PostProcessPass {
+//     #geometry;
+//     material;
+// 
+//     constructor({ gpu }) {
+//         this.#geometry = new PlaneGeometry({ gpu });
+//         this.material = new Material()
+//     }
+// }
