838269432e81179dd6896b4281eb5d9f2a512eac
takumifukasawa <takumifukasawa@gmail.com>
2022-11-10 20:00:58 +0900

    shadowpass, scenepass

--- workspace/014_engine/PaleGL/actors/Actor.js
+++ workspace/014_engine/PaleGL/actors/Actor.js
@@ -1,13 +1,16 @@
 ﻿import {Transform} from "../core/Transform.js";
 import {ActorTypes} from "./../constants.js";
+import {uuidv4} from "../utilities/uuid.js";
 
 export class Actor {
     transform = new Transform();
     type;
+    uuid;
     
     constructor(type) {
         this.transform.actor = this;
         this.type = type || ActorTypes.Null;
+        this.uuid = uuidv4();
     }
     
     addChild(child) {
--- workspace/014_engine/PaleGL/core/ForwardRenderer.js
+++ workspace/014_engine/PaleGL/core/ForwardRenderer.js
@@ -15,7 +15,7 @@ export class ForwardRenderer {
         this.#gpu = gpu;
         this.canvas = canvas;
         this.pixelRatio = pixelRatio;
-        
+
         this.#shadowMaterial = new Material({
             gpu,
             vertexShader: `#version 300 es
@@ -67,80 +67,22 @@ export class ForwardRenderer {
         this.#gpu.clear(r, g, b, a);
     }
 
-    render(scene, camera) {
-       
-        const meshActorsEachQueue = {
-            skybox: [], // maybe only one
-            opaque: [],
-            transparent: [],
-        };
-        const lightActors = [];
-
-        scene.traverse((actor) => {
-            switch(actor.type) {
-                case ActorTypes.Skybox:
-                    meshActorsEachQueue.skybox.push(actor);
-                    // actor.transform.parent = camera.transform;
-                    break;
-                case ActorTypes.Mesh:
-                    switch (actor.material.blendType) {
-                        case BlendTypes.Opaque:
-                            meshActorsEachQueue.opaque.push(actor);
-                            break;
-                        case BlendTypes.Transparent:
-                        case BlendTypes.Additive:
-                            meshActorsEachQueue.transparent.push(actor);
-                            break;
-                        default:
-                            throw "invalid blend type";
-                    }
-                    break;
-                case ActorTypes.Light:
-                    lightActors.push(actor);
-                    break;
-            }
-        });
-
-        // 本当はあんまりgpu渡したくないけど、渡しちゃったほうがいろいろと楽
-        scene.traverse((actor) => actor.update({ gpu: this.#gpu }));
-
-        // update all actors matrix
-        // TODO
-        // - scene 側でやった方がよい？
-        // - skyboxのupdateTransformが2回走っちゃうので、sceneかカメラに持たせて特別扱いさせたい
-        // - やっぱりcomponentシステムにした方が良い気もする
-        scene.traverse((actor) => actor.updateTransform());
-
-        scene.traverse((actor) => actor.afterUpdatedTransform());
-        
-        // TODO: depth sort 
-
-        // sort by render queue
-        const sortRenderQueueCompareFunc = (a, b) => a.material.renderQueue - b.material.renderQueue;
-        const sortedMeshActors = Object.keys(meshActorsEachQueue).map(key => (meshActorsEachQueue[key].sort(sortRenderQueueCompareFunc))).flat();
-        
-        // ------------------------------------------------------------------------------
-        // shadow pass
-        // ------------------------------------------------------------------------------
-
-        lightActors.forEach(lightActor => {
-            if(!lightActor.castShadow) {
-                return;
-            }
+    #shadowPass(castShadowLightActors, opaqueMeshActors) {
+        castShadowLightActors.forEach(lightActor => {
             this.setRenderTarget(lightActor.shadowMap.write);
             // console.log(lightActor.shadowMap.write())
             this.clear(0, 0, 0, 1);
-            
-            meshActorsEachQueue.opaque.forEach(meshActor => {
+
+            opaqueMeshActors.forEach(meshActor => {
                 // skybox は shadowmapに描画しない
-                switch(meshActor.type) {
+                switch (meshActor.type) {
                     case ActorTypes.Skybox:
                         return;
                 }
-                
+
                 // const material = meshActor.material;
                 const targetMaterial = this.#shadowMaterial;
-               
+
                 // TODO: material 側でやった方がよい？
                 if (targetMaterial.uniforms.uWorldMatrix) {
                     targetMaterial.uniforms.uWorldMatrix.value = meshActor.transform.worldMatrix;
@@ -151,21 +93,14 @@ export class ForwardRenderer {
                 if (targetMaterial.uniforms.uProjectionMatrix) {
                     targetMaterial.uniforms.uProjectionMatrix.value = lightActor.shadowCamera.projectionMatrix;
                 }
-               
+
                 this.renderMesh(meshActor.geometry, this.#shadowMaterial);
                 // this.renderMesh(meshActor.geometry, meshActor.material);
             });
         });
-
-        // ------------------------------------------------------------------------------
-        // scene pass
-        // ------------------------------------------------------------------------------
-     
-        if (camera.enabledPostProcess) {
-            this.setRenderTarget(camera.postProcess.renderTarget.write);
-        } else {
-            this.setRenderTarget(camera.renderTarget ? camera.renderTarget.write : null);
-        }
+    }
+    
+    #scenePass(sortedMeshActors, camera, lightActors) {
 
         // TODO: refactor
         this.clear(
@@ -174,17 +109,17 @@ export class ForwardRenderer {
             camera.clearColor.z,
             camera.clearColor.w
         );
-        
+
         sortedMeshActors.forEach(meshActor => {
-            switch(meshActor.type) {
+            switch (meshActor.type) {
                 case ActorTypes.Skybox:
                     // TODO: skyboxのupdateTransformが2回走っちゃうので、sceneかカメラに持たせて特別扱いさせたい
                     meshActor.updateTransform(camera);
                     break;
             }
-            
+
             const targetMaterial = meshActor.material;
-            
+
             // TODO: material 側でやった方がよい？
             if (targetMaterial.uniforms.uWorldMatrix) {
                 targetMaterial.uniforms.uWorldMatrix.value = meshActor.transform.worldMatrix;
@@ -198,7 +133,7 @@ export class ForwardRenderer {
             if (targetMaterial.uniforms.uNormalMatrix) {
                 targetMaterial.uniforms.uNormalMatrix.value = meshActor.transform.worldMatrix.clone().invert().transpose();
             }
-            if(targetMaterial.uniforms.uViewPosition) {
+            if (targetMaterial.uniforms.uViewPosition) {
                 targetMaterial.uniforms.uViewPosition.value = camera.transform.worldMatrix.position;
             }
 
@@ -225,21 +160,21 @@ export class ForwardRenderer {
                         }
                     }
                 }
-                
-                if(targetMaterial.uniforms.uShadowMapProjectionMatrix && light.shadowCamera) {
+
+                if (targetMaterial.uniforms.uShadowMapProjectionMatrix && light.shadowCamera) {
                     // clip coord (-1 ~ 1) to uv (0 ~ 1)
                     const textureMatrix = new Matrix4(
-                       0.5, 0, 0, 0.5,
-                       0, 0.5, 0, 0.5,
-                       0, 0, 0.5, 0.5,
-                       0, 0, 0, 1
+                        0.5, 0, 0, 0.5,
+                        0, 0.5, 0, 0.5,
+                        0, 0, 0.5, 0.5,
+                        0, 0, 0, 1
                     );
                     const textureProjectionMatrix = Matrix4.multiplyMatrices(
                         textureMatrix,
                         light.shadowCamera.projectionMatrix.clone(),
                         light.shadowCamera.viewMatrix.clone()
                     );
-                    
+
                     // TODO: directional light の構造体に持たせた方がいいかもしれない
                     targetMaterial.uniforms.uShadowMapProjectionMatrix.value = textureProjectionMatrix;
                 }
@@ -247,11 +182,85 @@ export class ForwardRenderer {
 
             this.renderMesh(meshActor.geometry, meshActor.material);
         });
+    }
+
+    render(scene, camera) {
+        const meshActorsEachQueue = {
+            skybox: [], // maybe only one
+            opaque: [],
+            transparent: [],
+        };
+        const lightActors = [];
+
+        scene.traverse((actor) => {
+            switch (actor.type) {
+                case ActorTypes.Skybox:
+                    meshActorsEachQueue.skybox.push(actor);
+                    // actor.transform.parent = camera.transform;
+                    break;
+                case ActorTypes.Mesh:
+                    switch (actor.material.blendType) {
+                        case BlendTypes.Opaque:
+                            meshActorsEachQueue.opaque.push(actor);
+                            break;
+                        case BlendTypes.Transparent:
+                        case BlendTypes.Additive:
+                            meshActorsEachQueue.transparent.push(actor);
+                            break;
+                        default:
+                            throw "invalid blend type";
+                    }
+                    break;
+                case ActorTypes.Light:
+                    lightActors.push(actor);
+                    break;
+            }
+        });
+
+        // 本当はあんまりgpu渡したくないけど、渡しちゃったほうがいろいろと楽
+        scene.traverse((actor) => actor.update({gpu: this.#gpu}));
+
+        // update all actors matrix
+        // TODO
+        // - scene 側でやった方がよい？
+        // - skyboxのupdateTransformが2回走っちゃうので、sceneかカメラに持たせて特別扱いさせたい
+        // - やっぱりcomponentシステムにした方が良い気もする
+        scene.traverse((actor) => actor.updateTransform());
+
+        scene.traverse((actor) => actor.afterUpdatedTransform());
+
+        // TODO: depth sort 
+
+        // sort by render queue
+        const sortRenderQueueCompareFunc = (a, b) => a.material.renderQueue - b.material.renderQueue;
+        const sortedMeshActors = Object.keys(meshActorsEachQueue).map(key => (meshActorsEachQueue[key].sort(sortRenderQueueCompareFunc))).flat();
+
+        // ------------------------------------------------------------------------------
+        // 1. shadow pass
+        // ------------------------------------------------------------------------------
+      
+        const castShadowLightActors = lightActors.filter(lightActor => lightActor.castShadow);
+        
+        if(castShadowLightActors.length > 0 && meshActorsEachQueue.opaque.length > 0) {
+            this.#shadowPass(castShadowLightActors, meshActorsEachQueue.opaque);
+        }
+
+        // ------------------------------------------------------------------------------
+        // 2. scene pass
+        // ------------------------------------------------------------------------------
+        
+        if (camera.enabledPostProcess) {
+            this.setRenderTarget(camera.postProcess.renderTarget.write);
+        } else {
+            this.setRenderTarget(camera.renderTarget ? camera.renderTarget.write : null);
+        }
+       
+        this.#scenePass(sortedMeshActors, camera, lightActors);
 
         if (camera.enabledPostProcess) {
             camera.postProcess.render(this, camera);
         }
-       
+
         // NOTE: ない方がよい？
         // this.setRenderTarget(null);
     }
@@ -268,13 +277,13 @@ export class ForwardRenderer {
         this.#gpu.setShader(material.shader);
         // uniforms
         this.#gpu.setUniforms(material.uniforms);
-      
+
         // setup depth write (depth mask)
         let depthWrite;
-        if(material.depthWrite !== null) {
+        if (material.depthWrite !== null) {
             depthWrite = material.depthWrite;
         } else {
-            switch(material.blendType) {
+            switch (material.blendType) {
                 case BlendTypes.Opaque:
                     depthWrite = true;
                     break;
@@ -286,10 +295,10 @@ export class ForwardRenderer {
                     throw "invalid depth write";
             }
         }
-        
+
         // setup depth test
         const depthTest = material.depthTest;
-        
+
         // draw
         this.#gpu.draw(
             geometry.drawCount,
