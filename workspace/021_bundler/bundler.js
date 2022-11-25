77c0211fb4c5f4c2a7c10d29617e00dfa1d13773
takumifukasawa <takumifukasawa@gmail.com>
2022-11-25 18:16:50 +0900

    try watch files

---
+++ workspace/021_bundler/bundler.js
@@ -0,0 +1,12 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+const watchPath = path.join(__dirname, "PaleGL");
+
+fs.watchFile(watchPath, {
+    persistent: true,
+    recursive: true,
+    interval: 100,
+}, (current, prev) => {
+    console.log(current, prev)
+})
