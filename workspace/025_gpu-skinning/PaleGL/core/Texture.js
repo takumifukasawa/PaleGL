13625482dbf00184280116f756b5dfa6f214e4b5
takumifukasawa <takumifukasawa@gmail.com>
2022-12-04 15:12:44 +0900

    [wip] gpu skinning project

---
+++ workspace/025_gpu-skinning/PaleGL/core/Texture.js
@@ -0,0 +1,160 @@
+﻿import {GLObject} from "./GLObject.js";
+import {TextureFilterTypes, TextureTypes, TextureWrapTypes} from "./../constants.js";
+
+// TODO: texStorage2Dを使う場合と出し分ける
+export class Texture extends GLObject {
+    #texture;
+    #img;
+    #gpu;
+    type;
+
+    get glObject() {
+        return this.#texture;
+    }
+
+    constructor({
+        gpu,
+        img,
+        type = TextureTypes.RGBA,
+        width, height,
+        mipmap = false,
+        minFilter = TextureFilterTypes.Nearest, magFilter = TextureFilterTypes.Nearest,
+        wrapS = TextureWrapTypes.ClampToEdge, wrapT = TextureWrapTypes.ClampToEdge,
+        flipY = false,
+    }) {
+        super();
+        
+        this.type = type;
+
+        this.#gpu = gpu;
+        const gl = this.#gpu.gl;
+
+        this.#img = img || null;
+
+        this.#texture = gl.createTexture();
+
+        // bind texture object to gl
+        gl.bindTexture(gl.TEXTURE_2D, this.#texture);
+
+        // mipmap settings
+        if (mipmap) {
+            gl.generateMipmap(gl.TEXTURE_2D);
+        }
+      
+        // filter
+        switch(this.type) {
+            case TextureTypes.RGBA:
+                // min filter settings
+                switch(minFilter) {
+                    case TextureFilterTypes.Nearest:
+                        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+                        break;
+                    case TextureFilterTypes.Linear:
+                        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+                        break;
+                    default:
+                        throw "invalid min filter type"
+                }
+                // mag filter settings
+                switch(magFilter) {
+                    case TextureFilterTypes.Nearest:
+                        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+                        break;
+                    case TextureFilterTypes.Linear:
+                        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+                        break;
+                    default:
+                        throw "invalid mag filter type"
+                }
+                break;
+               
+            // TODO: depthの場合nearest必須？
+            case TextureTypes.Depth:
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+                break;
+            default:
+                throw "invalid texture type";
+        }
+        
+        // wrap settings
+        switch(wrapS) {
+            case TextureWrapTypes.ClampToEdge:
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                break;
+            case TextureWrapTypes.Repeat:
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+                break;
+        }
+        switch(wrapT) {
+            case TextureWrapTypes.ClampToEdge:
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                break;
+            case TextureWrapTypes.Repeat:
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+                break;
+        }
+
+        if (!!this.#img || flipY) {
+            // uv座標そのものは左下からなのでglもそれに合わせるためにflip
+            // html image coord -> gl texture coord
+            // (0, 0) - (1, 0)     (0, 1) - (1, 1)
+            //   |         |         |         |
+            // (0, 1) - (1, 1)     (0, 0) - (1, 0)
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        } else {
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+        }
+
+        // bind texture data
+        switch(this.type) {
+            case TextureTypes.RGBA:
+                if (width && height) {
+                    // for render target
+                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, this.#img);
+                } else {
+                    // set img to texture
+                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.#img);
+                }
+                break;
+            case TextureTypes.Depth:
+                if (width && height) {
+                    // for render target
+                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.DEPTH_COMPONENT32F, width, height, 0, gl.DEPTH_COMPONENT, gl.FLOAT, this.#img);
+                } else {
+                    // set img to texture
+                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.DEPTH_COMPONENT32F, gl.DEPTH_COMPONENT, gl.FLOAT, this.#img);
+                }
+                break;
+            default:
+                throw "invalid type";
+        }
+       
+        // TODO: あった方がよい？
+        // unbind img
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+    
+    setSize(width, height) {
+        const gl = this.#gpu.gl;
+        gl.bindTexture(gl.TEXTURE_2D, this.#texture);
+
+        // bind texture data
+        switch(this.type) {
+            case TextureTypes.RGBA:
+                // for render target
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, this.#img);
+                break;
+            case TextureTypes.Depth:
+                // for render target
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.DEPTH_COMPONENT32F, width, height, 0, gl.DEPTH_COMPONENT, gl.FLOAT, this.#img);
+                break;
+            default:
+                throw "invalid type";
+        }
+        
+        // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, this.#img);
+        
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+}
