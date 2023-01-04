1d2b2bfdebcf312f954f9bff288f9bef75e599aa
takumifukasawa <takumifukasawa.com>
2023-01-04 19:41:43 +0900

    input etc...

---
+++ workspace/031_soft-particle/PaleGL/core/OrbitCameraController.js
@@ -0,0 +1,74 @@
+import {Vector3} from "../math/Vector3.js";
+import {clamp} from "../utilities/mathUtilities.js";
+
+export class OrbitCameraController {
+    #camera;
+    dampingFactor = 0.01;
+    minAzimuth;
+    maxAzimuth;
+    minAltitude = -45;
+    maxAltitude = 45;
+    azimuthSpeed = 100;
+    altitudeSpeed = 100;
+    #cameraAngle = { azimuth: 0, altitude: 0};
+    #lookAtTarget = Vector3.zero();
+    distance = 10;
+    attenuation = 0.001;
+    #targetX;
+    #targetY;
+    
+    #targetCameraPosition = Vector3.zero();
+    #currentCameraPosition = Vector3.zero();
+    
+    set lookAtTarget(v) {
+        this.#lookAtTarget = v;
+    }
+    
+    constructor(camera) {
+        this.#camera = camera;
+    }
+    
+    start(defaultAzimuth = 0, defaultAltitude = 0) {
+        this.#cameraAngle.azimuth = defaultAzimuth;
+        this.#cameraAngle.altitude = defaultAltitude;
+        this.#updateCameraPosition(true);
+        // this.#targetCameraPosition = new Vector3(0, 0, this.distance);
+        // this.#currentCameraPosition = this.#targetCameraPosition.clone();
+    }
+    
+    setDelta(deltaX, deltaY) {
+        this.#targetX = deltaX;
+        this.#targetY = deltaY;
+    }
+
+    fixedUpdate() {
+        this.#targetX = Math.sign(this.#targetX) * Math.max(0, Math.abs(this.#targetX) - this.attenuation);
+        this.#targetY = Math.sign(this.#targetY) * Math.max(0, Math.abs(this.#targetY) - this.attenuation);
+        
+        this.#cameraAngle.azimuth += this.#targetX * this.azimuthSpeed;
+        this.#cameraAngle.altitude += this.#targetY * this.altitudeSpeed;
+        
+        this.#updateCameraPosition();
+    }
+
+    #updateCameraPosition(isJump = false) {
+        // TODO: limit azimuth
+        this.#cameraAngle.azimuth = this.#cameraAngle.azimuth % 360;
+        this.#cameraAngle.altitude = clamp(this.#cameraAngle.altitude, this.minAltitude, this.maxAltitude);
+
+        const v1 = Vector3.rotateVectorX(new Vector3(0, 0, 1), this.#cameraAngle.altitude);
+        const v2 = Vector3.rotateVectorY(v1, this.#cameraAngle.azimuth);
+        this.#targetCameraPosition = Vector3.addVectors(
+            this.#lookAtTarget,
+            v2.scale(this.distance)
+        );
+        this.#currentCameraPosition = Vector3.lerpVectors(
+            this.#currentCameraPosition,
+            this.#targetCameraPosition,
+            isJump ? 1 : this.dampingFactor
+        );
+
+        this.#camera.transform.position = this.#currentCameraPosition;
+        this.#camera.transform.lookAt(this.#lookAtTarget);
+    }
+}
---
+++ workspace/031_soft-particle/PaleGL/inputs/AbstractInputController.js
@@ -0,0 +1,116 @@
+import {Vector2} from "../math/Vector2.js";
+
+export class AbstractInputController {
+    #_beforeInputPosition = Vector2.zero;
+    #_currentInputPosition = Vector2.zero;
+    #_deltaInputPosition = Vector2.zero;
+    #_deltaNormalizedInputPosition = Vector2.zero;
+    
+    #_isPressed = false;
+    #_isDown = false;
+    #_isReleased = false;
+    
+    #_width;
+    #_height;
+    
+    get isUp() {
+        return !this.#_isDown;
+    }
+    
+    get isPressed() {
+        return this.#_isPressed;
+    }
+
+    get isDown() {
+        return this.#_isDown;
+    }
+    
+    get isReleased() {
+        return this.#_isReleased;
+    }
+    
+    get deltaNormalizedInputPosition() {
+        return this.#_deltaNormalizedInputPosition;
+    }
+
+    constructor() {
+        this.clearInputPositions();
+    }
+
+    start() {
+        throw "[AbstractInputController] should implementation 'start' method.";
+    }
+    
+    setSize(width, height) {
+        this.#_width = width;
+        this.#_height = height;
+    }
+
+    // inputPosition ... v2
+    // isDown ... bool
+    updateInternal({ inputPosition, isDown }) {
+        this.#updateState(isDown);
+        this.#updateInputPositions(inputPosition);
+    }
+   
+    #updateState(isDown) {
+        const isBeforeDown = this.isDown;
+        this.#_isDown = isDown;
+
+        // pressed
+        if(!isBeforeDown && this.isDown) {
+            this.#_isPressed = true;
+            this.#_isReleased = false;
+            return;
+        }
+        // down
+        if(!isBeforeDown && this.isDown) {
+            this.#_isPressed = false;
+            this.#_isReleased = false;
+            return;
+        }
+        // released
+        if(isBeforeDown && !this.isDown) {
+            this.#_isPressed = false;
+            this.#_isReleased = true;
+            return;
+        }
+        // up 
+        this.#_isPressed = false;
+        this.#_isReleased = false;
+    }
+
+    #updateInputPositions(inputPosition) {
+        // up
+        if(this.isUp) {
+            this.clearInputPositions();
+            return;
+        }
+        // pressed
+        if(this.isPressed) {
+            this.#_currentInputPosition.copy(inputPosition);
+            this.#_beforeInputPosition.copy(this.#_currentInputPosition);
+            this.#_deltaInputPosition.set(0, 0);
+            this.#_deltaNormalizedInputPosition.set(0, 0);
+            return;
+        }
+        // move
+        this.#_beforeInputPosition.copy(this.#_currentInputPosition);
+        this.#_currentInputPosition.copy(inputPosition);
+        const diff = Vector2.subVectors(this.#_currentInputPosition, this.#_beforeInputPosition);
+        this.#_deltaInputPosition.copy(diff);
+        this.#_deltaNormalizedInputPosition.set(
+            this.#_deltaInputPosition.x / this.#_width,
+            this.#_deltaInputPosition.y / this.#_height
+        );
+    }
+  
+    clearInputPositions() {
+        this.#_beforeInputPosition.set(-Infinity, -Infinity);
+        this.#_currentInputPosition.set(-Infinity, -Infinity);
+        this.#_deltaInputPosition.set(-Infinity, -Infinity);
+        this.#_deltaNormalizedInputPosition.set(-Infinity, -Infinity);
+    }
+
+    dispose() {}
+}
