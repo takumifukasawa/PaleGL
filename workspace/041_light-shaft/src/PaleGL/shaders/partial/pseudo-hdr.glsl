9888b0a0082e496b02e16dd4ae3b00756eeae5b3
takumifukasawa <takumifukasawa@gmail.com>
2023-09-17 21:39:52 +0900

    create light shaft project

---
+++ workspace/041_light-shaft/src/PaleGL/shaders/partial/pseudo-hdr.glsl
@@ -0,0 +1,26 @@
+﻿
+// ref: https://game.watch.impress.co.jp/docs/20081203/3dmg4.htm
+
+// hdr rgb -> 8bit color
+vec4 encodePseudoHDR(vec3 color) {
+    float base = .25;
+    float l = max(max(color.r, color.g), max(color.b, base));
+    return vec4(
+        color.r / l,
+        color.g / l,
+        color.b / l,
+        base / l
+    );
+}
+
+// rgb: hdr color, alpha: luma
+vec4 decodePseudoHDR(vec4 encodedColor) {
+    float base = .25;
+    float rl = encodedColor.a;
+    return vec4(
+        (encodedColor.r / rl) * base,
+        (encodedColor.g / rl) * base,
+        (encodedColor.b / rl) * base,
+        rl * base
+    );
+}
