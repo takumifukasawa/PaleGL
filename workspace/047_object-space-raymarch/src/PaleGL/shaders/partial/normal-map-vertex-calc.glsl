88baf2e77cdb4cef86a2fae8e0ac5cd7d5060887
takumifukasawa <takumifukasawa@gmail.com>
2023-12-19 23:47:52 +0900

    fix condition

--- workspace/047_object-space-raymarch/src/PaleGL/shaders/partial/normal-map-vertex-calc.glsl
+++ workspace/047_object-space-raymarch/src/PaleGL/shaders/partial/normal-map-vertex-calc.glsl
@@ -1,35 +1,37 @@
 // TODO: インスタンスごとの法線行列もあらかじめ計算しておけるとなおよい
 
+mat3 normalMatrix;
+
 #ifdef USE_NORMAL_MAP
     #if defined(USE_SKINNING_GPU) || defined(USE_SKINNING_CPU)
         #ifdef USE_INSTANCING
-            vNormal = mat3(transpose(inverse(worldMatrix))) * mat3(skinMatrix) * aNormal;
+            normalMatrix = mat3(transpose(inverse(worldMatrix))) * mat3(skinMatrix);
         #else
-            vNormal = mat3(uNormalMatrix) * mat3(skinMatrix) * aNormal;
+            normalMatrix = mat3(uNormalMatrix) * mat3(skinMatrix);
         #endif
-        vTangent = mat3(uNormalMatrix) * mat3(skinMatrix) * aTangent;
-        vBinormal = mat3(uNormalMatrix) * mat3(skinMatrix) * aBinormal;
     #else
         #ifdef USE_INSTANCING
-            vNormal = mat3(transpose(inverse(worldMatrix))) * aNormal;
+            normalMatrix = mat3(transpose(inverse(worldMatrix)));
         #else
-            vNormal = mat3(uNormalMatrix) * aNormal;
+            normalMatrix = mat3(uNormalMatrix);
         #endif
-        vTangent = mat3(uNormalMatrix) * aTangent;
-        vBinormal = mat3(uNormalMatrix) * aBinormal;
     #endif
+    vNormal = normalMatrix * aNormal;
+    vTangent = normalMatrix * aTangent;
+    vBinormal = normalMatrix * aBinormal;
 #else
     #if defined(USE_SKINNING_GPU) || defined(USE_SKINNING_CPU)
         #ifdef USE_INSTANCING
-            vNormal = mat3(transpose(inverse(worldMatrix))) * mat3(skinMatrix) * aNormal;
+            normalMatrix = mat3(transpose(inverse(worldMatrix))) * mat3(skinMatrix);
         #else
-            vNormal = mat3(uNormalMatrix) * mat3(skinMatrix) * aNormal;
+            normalMatrix = mat3(uNormalMatrix);
         #endif
     #else
         #ifdef USE_INSTANCING
-            vNormal = mat3(transpose(inverse(worldMatrix))) * aNormal;
+            normalMatrix = mat3(transpose(inverse(worldMatrix)));
         #else
-            vNormal = mat3(uNormalMatrix) * aNormal;
+            normalMatrix = mat3(uNormalMatrix);
         #endif
     #endif
+    vNormal = normalMatrix * aNormal;
 #endif
