d00a140b2533a9235a22bd4d902f7c10fd62a94b
takumifukasawa <takumifukasawa@gmail.com>
2023-12-23 21:13:42 +0900

    layout glsl

---
+++ workspace/047_object-space-raymarch/src/PaleGL/shaders/layout-gbuffer-object-space-raymarch-depth-fragment.glsl
@@ -0,0 +1,85 @@
+#version 300 es
+
+precision highp float;
+
+#pragma DEFINES
+
+// raymarch
+#include ./partial/raymarch-distance-functions.glsl
+
+#pragma BLOCK_RAYMARCH_SCENE
+
+#include ./partial/raymarch-utility-functions.glsl
+
+#include ./partial/alpha-test-functions.glsl
+
+uniform vec4 uColor;
+uniform sampler2D uDiffuseMap;
+uniform vec2 uDiffuseMapUvScale;
+uniform vec3 uViewPosition;
+uniform mat4 uWorldMatrix;
+uniform mat4 uInverseWorldMatrix;
+uniform vec3 uBoundsScale;
+
+#ifdef USE_ALPHA_TEST
+uniform float uAlphaTestThreshold;
+#endif
+
+in vec2 vUv;
+in vec3 vWorldPosition;
+
+#ifdef USE_VERTEX_COLOR
+in vec4 vVertexColor;
+#endif
+
+out vec4 outColor;
+
+void main() {
+    vec2 uv = vUv * uDiffuseMapUvScale;
+
+    vec4 diffuseMapColor = texture(uDiffuseMap, uv);
+
+    vec4 diffuseColor = vec4(0.);
+
+    #ifdef USE_VERTEX_COLOR
+    diffuseColor = vVertexColor * uColor * diffuseMapColor;
+    #else
+    diffuseColor = uColor * diffuseMapColor;
+    #endif
+
+    //
+    // NOTE: raymarch block
+    //
+
+    vec3 rayOrigin = vWorldPosition;
+    vec3 rayDirection = normalize(vWorldPosition - uViewPosition);
+    float distance = 0.;
+    float accLen = 0.;
+    vec3 currentRayPosition = rayOrigin;
+    float minDistance = .0001;
+    for(int i = 0; i < 64; i++) {
+        currentRayPosition = rayOrigin + rayDirection * accLen;
+        distance = objectSpaceDfScene(currentRayPosition, uInverseWorldMatrix, uBoundsScale);
+        accLen += distance;
+        if(
+        !isDfInnerBox(toLocal(currentRayPosition, uInverseWorldMatrix, uBoundsScale), uBoundsScale) ||
+        distance <= minDistance
+        ) {
+            break;
+        }
+    }
+    if(distance > minDistance) {
+        discard;
+    }
+
+    //
+    // NOTE: end raymarch block
+    //
+
+
+    float alpha = diffuseColor.a; // TODO: base color を渡して alpha をかける
+
+    #include ./partial/alpha-test-calc.glsl
+
+    outColor = vec4(1., 1., 1., 1.);
+}
--- workspace/047_object-space-raymarch/vite.config.ts
+++ workspace/047_object-space-raymarch/vite.config.ts
@@ -31,6 +31,62 @@ export const deleteTmpCachesPlugin: () => Plugin = () => {
     };
 };
 
+/**
+ *
+ * #pragma BLOCK_***_START ~~ #pragma BLOCK_***_END を block として、その中身を挿入する
+ */
+export const transformGlslLayout: () => Plugin = () => {
+    return {
+        name: 'transform-glsl-layout',
+        enforce: 'pre',
+        async transform(src: string, id: string) {
+            const fileRegex = /\.glsl$/;
+            if (fileRegex.test(id)) {
+                const blockSrcRegex = /#pragma BLOCK_([A-Z_]*?)_START([\s\S]*?)#pragma BLOCK_[A-Z_]*?_END/g;
+                const blockSrcMatches = [...src.matchAll(blockSrcRegex)];
+                
+                // for debug
+                // const originalSrc = src;
+                // console.log(`[transform-glsl-layout] target - id: ${id}`);
+                // console.log("---------------------------------")
+                // console.log(originalSrc)
+                // console.log("---------------------------------")
+                // console.log(blockSrcMatches)
+                
+                for (let i = 0; i < blockSrcMatches.length; i++) {
+                    const [matchContent, blockName, blockContent] = blockSrcMatches[i];
+                    const blockDestRegex = new RegExp(`#pragma BLOCK_${blockName}`, 'g');
+                        
+                    // for debug
+                    // console.log("---------------------------------")
+                    // console.log("matchContent: ", matchContent);
+                    // console.log("blockName: ", blockName);
+                    // console.log("blockContent: ", blockContent);
+                    // console.log("---------------------------------")
+                    // console.log([...src.matchAll(blockDestRegex)]);
+                    // console.log("---------------------------------")
+                    // console.log("match block content: ", src.match(new RegExp(blockContent, 'g')));
+              
+                    // blockの囲い含めすべて消す
+                    src = src.replaceAll(matchContent, "");
+                    // 消した後、block内の記述を挿入（置き換え）
+                    src = src.replaceAll(blockDestRegex, blockContent);
+                }
+                
+                // for debug
+                // console.log("---------------------------------")
+                // console.log("original src: ------------------------------")
+                // console.log(originalSrc)
+                // console.log("result: ------------------------------")
+                // console.log(src)
+                
+                return src;
+            }
+            return src;
+        },
+    };
+};
+
 // ref:
 // https://ja.vitejs.dev/config/
 // https://github.com/vitejs/vite/issues/621
@@ -64,6 +120,7 @@ export default defineConfig(({ mode }) => {
                 exclude: undefined,
                 compress: false,
             }),
+            transformGlslLayout(),
             shaderMinifierPlugin({
                 minify: isMinifyShader,
                 minifierOptions: {
