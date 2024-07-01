ee6a3ecaded929d1a06999bfb7c028268c0ce3cc
takumifukasawa <takumifukasawa.com>
2024-07-01 10:28:31 +0900

    wip shared texture pp 2

--- workspace/053_noise/src/PaleGL/core/createSharedTextures.ts
+++ workspace/053_noise/src/PaleGL/core/createSharedTextures.ts
@@ -17,59 +17,20 @@ export const SharedTexturesTypes = {
 export type SharedTexturesType = (typeof SharedTexturesTypes)[keyof typeof SharedTexturesTypes];
 
 export type SharedTextures = {
-    [SharedTexturesTypes.RANDOM_NOISE]: Texture;
-};
+    key: SharedTexturesType;
+    // tmpRenderTarget: RenderTarget;
+    // renderTarget: RenderTarget;
+    texture: Texture;
+    update: () => void | null;
+}[];
 
-// const sharedTextureInfos = [
-//     {
-//         width: 512,
-//         height: 512,
-//         material: new Material({
-//             vertexShader: PostProcessPassBase.baseVertexShader,
-//             fragmentShader: randomNoiseFragment,
-//             uniforms: [
-//                 {
-//                     name: 'uTime',
-//                     type: UniformTypes.Float,
-//                     value: 0,
-//                 },
-//                 {
-//                     name: 'uGridSize',
-//                     type: UniformTypes.Vector2,
-//                     value: new Vector2(512, 512),
-//                 },
-//             ],
-//         })
-//     }
-// ];
-
-export function createSharedTextures({ gpu, renderer }: { gpu: GPU; renderer: Renderer }): SharedTextures {
-    const planeGeometry = new PlaneGeometry({ gpu });
-
-    const createEffectRenderTarget = ({ gpu, size }: { gpu: GPU; size: number }) => {
-        return new RenderTarget({
-            gpu,
-            width: size,
-            height: size,
-            type: RenderTargetTypes.RGBA,
-            minFilter: TextureFilterTypes.Linear,
-            magFilter: TextureFilterTypes.Linear,
-            wrapS: TextureWrapTypes.Repeat,
-            wrapT: TextureWrapTypes.Repeat,
-        });
-    };
-
-    const renderMaterial = (renderTarget: RenderTarget, material: Material) => {
-        renderer.setRenderTarget(renderTarget, true);
-        renderer.renderMesh(planeGeometry, material);
-        renderer.setRenderTarget(null);
-    };
-
-    const randomNoiseRenderTarget = createEffectRenderTarget({ gpu, size: 512 });
-    const randomNoiseMaterial = new Material({
-        vertexShader: PostProcessPassBase.baseVertexShader,
-        fragmentShader: randomNoiseFragment,
-        uniforms: [
+const sharedTextureInfos = [
+    {
+        key: SharedTexturesTypes.RANDOM_NOISE,
+        width: 512,
+        height: 512,
+        effectFragmentShader: randomNoiseFragment,
+        effectUniforms: [
             {
                 name: 'uTime',
                 type: UniformTypes.Float,
@@ -81,18 +42,8 @@ export function createSharedTextures({ gpu, renderer }: { gpu: GPU; renderer: Re
                 value: new Vector2(512, 512),
             },
         ],
-    });
-
-    const postProcessRenderTarget = createEffectRenderTarget({ gpu, size: 512 });
-    const postprocessMaterial = new Material({
-        vertexShader: PostProcessPassBase.baseVertexShader,
-        fragmentShader: effectTexturePostProcessFragment,
-        uniforms: [
-            {
-                name: 'uSrcTexture',
-                type: UniformTypes.Texture,
-                value: null,
-            },
+        postprocessFragmentShader: effectTexturePostProcessFragment,
+        postprocessUniforms: [
             {
                 name: 'uTilingEnabled',
                 type: UniformTypes.Float,
@@ -114,21 +65,141 @@ export function createSharedTextures({ gpu, renderer }: { gpu: GPU; renderer: Re
                 value: 1,
             },
         ],
-    });
-    
+    },
+];
+
+export function createSharedTextures({ gpu, renderer }: { gpu: GPU; renderer: Renderer }): SharedTextures {
+    const planeGeometry = new PlaneGeometry({ gpu });
+
+    const createEffectRenderTarget = ({ gpu, width, height }: { gpu: GPU; width: number; height: number }) => {
+        return new RenderTarget({
+            gpu,
+            width,
+            height,
+            type: RenderTargetTypes.RGBA,
+            minFilter: TextureFilterTypes.Linear,
+            magFilter: TextureFilterTypes.Linear,
+            wrapS: TextureWrapTypes.Repeat,
+            wrapT: TextureWrapTypes.Repeat,
+        });
+    };
+
+    const renderMaterial = (renderTarget: RenderTarget, material: Material) => {
+        renderer.setRenderTarget(renderTarget, true);
+        renderer.renderMesh(planeGeometry, material);
+        renderer.setRenderTarget(null);
+    };
+
     const planeGeometryAttributeDescriptors = planeGeometry.getAttributeDescriptors();
 
-    randomNoiseMaterial.start({ gpu, attributeDescriptors: planeGeometryAttributeDescriptors });
-    renderMaterial(randomNoiseRenderTarget, randomNoiseMaterial);
+    const sharedTextures: SharedTextures = sharedTextureInfos.map(
+        ({
+            key,
+            width,
+            height,
+            effectFragmentShader,
+            effectUniforms,
+            postprocessFragmentShader,
+            postprocessUniforms,
+        }) => {
+            const tmpRenderTarget = createEffectRenderTarget({ gpu, width, height });
+            const ppRenderTarget = createEffectRenderTarget({ gpu, width, height });
+            const tmpMaterial = new Material({
+                vertexShader: PostProcessPassBase.baseVertexShader,
+                fragmentShader: effectFragmentShader,
+                uniforms: effectUniforms,
+            });
+            const ppMaterial = new Material({
+                vertexShader: PostProcessPassBase.baseVertexShader,
+                fragmentShader: postprocessFragmentShader,
+                uniforms: [
+                    {
+                        name: 'uSrcTexture',
+                        type: UniformTypes.Texture,
+                        value: null,
+                    },
+                    ...postprocessUniforms,
+                ],
+            });
 
-    postprocessMaterial.start({ gpu, attributeDescriptors: planeGeometryAttributeDescriptors });
-    postprocessMaterial.uniforms.setValue('uSrcTexture', randomNoiseRenderTarget.texture);
+            tmpMaterial.start({ gpu, attributeDescriptors: planeGeometryAttributeDescriptors });
+            renderMaterial(tmpRenderTarget, tmpMaterial);
 
-    renderMaterial(postProcessRenderTarget, postprocessMaterial);
+            ppMaterial.start({ gpu, attributeDescriptors: planeGeometryAttributeDescriptors });
+            ppMaterial.uniforms.setValue('uSrcTexture', tmpRenderTarget.texture);
 
-    const sharedTextures: SharedTextures = {
-        [SharedTexturesTypes.RANDOM_NOISE]: postProcessRenderTarget.texture!,
-    };
+            renderMaterial(ppRenderTarget, ppMaterial);
+
+            return {
+                key,
+                texture: ppRenderTarget.texture!,
+                update: () => {},
+            };
+        }
+    );
+
+    // const randomNoiseRenderTarget = createEffectRenderTarget({ gpu, size: 512 });
+    // const randomNoiseMaterial = new Material({
+    //     vertexShader: PostProcessPassBase.baseVertexShader,
+    //     fragmentShader: randomNoiseFragment,
+    //     uniforms: [
+    //         {
+    //             name: 'uTime',
+    //             type: UniformTypes.Float,
+    //             value: 0,
+    //         },
+    //         {
+    //             name: 'uGridSize',
+    //             type: UniformTypes.Vector2,
+    //             value: new Vector2(512, 512),
+    //         },
+    //     ],
+    // });
+
+    // const postProcessRenderTarget = createEffectRenderTarget({ gpu, width, height });
+    // const postprocessMaterial = new Material({
+    //     vertexShader: PostProcessPassBase.baseVertexShader,
+    //     fragmentShader: effectTexturePostProcessFragment,
+    //     uniforms: [
+    //         {
+    //             name: 'uSrcTexture',
+    //             type: UniformTypes.Texture,
+    //             value: null,
+    //         },
+    //         {
+    //             name: 'uTilingEnabled',
+    //             type: UniformTypes.Float,
+    //             value: 1,
+    //         },
+    //         {
+    //             name: 'uEdgeMaskMix',
+    //             type: UniformTypes.Float,
+    //             value: 1,
+    //         },
+    //         {
+    //             name: 'uRemapMin',
+    //             type: UniformTypes.Float,
+    //             value: 0,
+    //         },
+    //         {
+    //             name: 'uRemapMax',
+    //             type: UniformTypes.Float,
+    //             value: 1,
+    //         },
+    //     ],
+    // });
+
+    // randomNoiseMaterial.start({ gpu, attributeDescriptors: planeGeometryAttributeDescriptors });
+    // renderMaterial(randomNoiseRenderTarget, randomNoiseMaterial);
+
+    // postprocessMaterial.start({ gpu, attributeDescriptors: planeGeometryAttributeDescriptors });
+    // postprocessMaterial.uniforms.setValue('uSrcTexture', randomNoiseRenderTarget.texture);
+
+    // renderMaterial(postProcessRenderTarget, postprocessMaterial);
+
+    // const sharedTextures: SharedTextures = {
+    //     [SharedTexturesTypes.RANDOM_NOISE]: postProcessRenderTarget.texture!,
+    // };
 
     return sharedTextures;
 }
