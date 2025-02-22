3c33218e1e42ff69f0c72dd0cb721b24c81db82b
takumifukasawa <takumifukasawa@gmail.com>
2025-02-23 00:35:50 +0900

    partial skinning

--- src/PaleGL/shaders/gbuffer-vertex.glsl
+++ src/PaleGL/shaders/gbuffer-vertex.glsl
@@ -8,7 +8,7 @@
 #include <ub>
 #include <vcolor_vh>
 
-#include ./partial/skinning-vertex-functions.glsl
+
 
 // varyings
 out vec2 vUv;
@@ -36,8 +36,6 @@ out vec3 vBinormal;
 #endif
 
 #pragma APPEND_UNIFORMS
-        
-#include ./partial/skinning-vertex-uniforms.glsl
 
 mat4 getRotationXMat(float rad) {
     float c = cos(rad);
@@ -131,14 +129,129 @@ mat4 getLookAtMat(vec3 lookAt, vec3 p) {
     );
 }
 
+// start skinning ---------------------------------------------------
+
+// #ifdef USE_SKINNING
+#if defined(USE_SKINNING_GPU) || defined(USE_SKINNING_CPU)
+// tmp for cpu skinning
+// uniform mat4[${jointNum}] uJointMatrices;
+// uniform sampler2D ${UniformNames.JointTexture};
+uniform sampler2D uJointTexture;
+uniform int uBoneCount;
+uniform int uJointTextureColNum;
+uniform int uTotalFrameCount;
+
+mat4 calcSkinningMatrix(mat4 jointMat0, mat4 jointMat1, mat4 jointMat2, mat4 jointMat3, vec4 boneWeights) {
+    mat4 skinMatrix =
+    jointMat0 * aBoneWeights.x +
+    jointMat1 * aBoneWeights.y +
+    jointMat2 * aBoneWeights.z +
+    jointMat3 * aBoneWeights.w;
+    return skinMatrix;
+}
+// TODO: animation data をシェーダーに渡して複数アニメーションに対応させたい
+// struct SkinAnimationClipData {
+//     int beginIndex; 
+//     int frameCount;
+// };
+mat4 getJointMatrix(sampler2D jointTexture, uint jointIndex, int colNum) {
+    // horizontal
+    int colIndex = int(mod(float(jointIndex), float(colNum)));
+    // vertical
+    int rowIndex = int(floor(float(jointIndex) / float(colNum)));
+    mat4 jointMatrix = mat4(
+    // 1: boneの行列が1個ずつ縦に並んでいる場合
+    // texelFetch(jointTexture, ivec2(0, jointIndex), 0),
+    // texelFetch(jointTexture, ivec2(1, jointIndex), 0),
+    // texelFetch(jointTexture, ivec2(2, jointIndex), 0),
+    // texelFetch(jointTexture, ivec2(3, jointIndex), 0)
+    // 2: 適宜詰めている場合
+    texelFetch(jointTexture, ivec2(colIndex * 4 + 0, rowIndex), 0),
+    texelFetch(jointTexture, ivec2(colIndex * 4 + 1, rowIndex), 0),
+    texelFetch(jointTexture, ivec2(colIndex * 4 + 2, rowIndex), 0),
+    texelFetch(jointTexture, ivec2(colIndex * 4 + 3, rowIndex), 0)
+    );
+    return jointMatrix;
+}
+mat4 getJointMatrixGPUSkinning(
+    sampler2D jointTexture,
+    uint jointIndex,
+    int jointNum,
+    int currentSkinIndex,
+    int colNum,
+    int totalFrameCount,
+    float time,
+    float timeOffset
+) {
+    //
+    // TODO: play / pause
+    // TODO: playback speed
+    // TODO: アニメーションのフレーム指定
+    //
+
+    float offset = float(int(mod(floor(time + timeOffset), float(totalFrameCount))) * jointNum);
+    // horizontal
+    int colIndex = int(mod(float(jointIndex) + offset, float(colNum)));
+    // vertical
+    int rowIndex = int(floor(float(jointIndex) + offset / float(colNum)));
+
+    mat4 jointMatrix = mat4(
+        texelFetch(jointTexture, ivec2(colIndex * 4 + 0, rowIndex), 0),
+        texelFetch(jointTexture, ivec2(colIndex * 4 + 1, rowIndex), 0),
+        texelFetch(jointTexture, ivec2(colIndex * 4 + 2, rowIndex), 0),
+        texelFetch(jointTexture, ivec2(colIndex * 4 + 3, rowIndex), 0)
+    );
+    return jointMatrix;
+}
+#endif
+
+// end skinning ---------------------------------------------------
+
 void main() {
 
     #pragma BEGIN_MAIN
 
     vec4 localPosition = vec4(aPosition, 1.);
 
-    #include ./partial/skinning-vertex-calc.glsl;
-    
+    // start calc skinning ---------------------------------------------------
+    #if defined(USE_SKINNING_GPU) || defined(USE_SKINNING_CPU)
+        mat4 skinMatrix = mat4(
+            1., 0., 0., 0.,
+            0., 1., 0., 0.,
+            0., 0., 1., 0.,
+            0., 0., 0., 1.
+        );
+        #ifdef USE_SKINNING_GPU
+            float fps = 30.;
+            mat4 jointMatrix0 = getJointMatrixGPUSkinning(uJointTexture, aBoneIndices[0], uBoneCount, 0, uJointTextureColNum, uTotalFrameCount, uTime * fps, aInstanceAnimationOffset);
+            mat4 jointMatrix1 = getJointMatrixGPUSkinning(uJointTexture, aBoneIndices[1], uBoneCount, 0, uJointTextureColNum, uTotalFrameCount, uTime * fps, aInstanceAnimationOffset);
+            mat4 jointMatrix2 = getJointMatrixGPUSkinning(uJointTexture, aBoneIndices[2], uBoneCount, 0, uJointTextureColNum, uTotalFrameCount, uTime * fps, aInstanceAnimationOffset);
+            mat4 jointMatrix3 = getJointMatrixGPUSkinning(uJointTexture, aBoneIndices[3], uBoneCount, 0, uJointTextureColNum, uTotalFrameCount, uTime * fps, aInstanceAnimationOffset);
+            skinMatrix = calcSkinningMatrix(
+                jointMatrix0,
+                jointMatrix1,
+                jointMatrix2,
+                jointMatrix3,
+                aBoneWeights
+            );
+        #endif
+        #ifdef USE_SKINNING_CPU
+            mat4 jointMatrix0 = getJointMatrix(uJointTexture, aBoneIndices[0], uJointTextureColNum);
+            mat4 jointMatrix1 = getJointMatrix(uJointTexture, aBoneIndices[1], uJointTextureColNum);
+            mat4 jointMatrix2 = getJointMatrix(uJointTexture, aBoneIndices[2], uJointTextureColNum);
+            mat4 jointMatrix3 = getJointMatrix(uJointTexture, aBoneIndices[3], uJointTextureColNum);
+            skinMatrix = calcSkinningMatrix(
+                jointMatrix0,
+                jointMatrix1,
+                jointMatrix2,
+                jointMatrix3,
+                aBoneWeights
+            );
+        #endif
+        localPosition = skinMatrix * localPosition;
+    #endif
+    // end calc skinning ---------------------------------------------------
+
     #pragma LOCAL_POSITION_POST_PROCESS
 
     // assign common varyings 
