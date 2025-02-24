8b28d66495316874e8f9a6ae672b323ffbdfd652
takumifukasawa <takumifukasawa@gmail.com>
2025-02-24 14:57:21 +0900

    remove unused codes

--- src/PaleGL/inputs/mouseInputController.ts
+++ src/PaleGL/inputs/mouseInputController.ts
@@ -1,53 +1,6 @@
 import {createAbstractInputController} from '@/PaleGL/inputs/abstractInputController.ts';
 import { Vector2 } from '@/PaleGL/math/Vector2';
 
-// export class MouseInputController extends AbstractInputController {
-//     _tmpIsDown = false;
-//     _tmpInputPosition = Vector2.zero;
-// 
-//     constructor() {
-//         super();
-//     }
-// 
-//     start() {
-//         window.addEventListener('mousedown', this._onMouseDown.bind(this));
-//         window.addEventListener('mousemove', this._onMouseMove.bind(this));
-//         window.addEventListener('mouseup', this._onMouseUp.bind(this));
-//     }
-// 
-//     update() {
-//         this.updateInternal({
-//             inputPosition: this._tmpInputPosition,
-//             isDown: this._tmpIsDown,
-//         });
-//     }
-// 
-//     _onMouseDown(e: MouseEvent) {
-//         this._tmpIsDown = true;
-//         this.setInputPosition(e.clientX, e.clientY);
-//     }
-// 
-//     _onMouseMove(e: MouseEvent) {
-//         this.setInputPosition(e.clientX, e.clientY);
-//     }
-// 
-//     _onMouseUp(e: MouseEvent) {
-//         this._tmpIsDown = false;
-//         this.setInputPosition(e.clientX, e.clientY);
-//         // this.setInputPosition(-Infinity, -Infinity);
-//     }
-// 
-//     setInputPosition(x: number, y: number) {
-//         this._tmpInputPosition.set(x, y);
-//     }
-// 
-//     dispose() {
-//         window.removeEventListener('mousedown', this._onMouseDown.bind(this));
-//         window.removeEventListener('mousemove', this._onMouseMove.bind(this));
-//         window.removeEventListener('mouseup', this._onMouseUp.bind(this));
-//     }
-// }
-
 export function createMouseInputController() {
     let _tmpIsDown = false;
     const _tmpInputPosition = Vector2.zero;
--- src/PaleGL/inputs/touchInputController.ts
+++ src/PaleGL/inputs/touchInputController.ts
@@ -1,57 +1,6 @@
 import {createAbstractInputController} from '@/PaleGL/inputs/abstractInputController.ts';
 import { Vector2 } from '@/PaleGL/math/Vector2';
 
-// export class TouchInputController extends AbstractInputController {
-//     _tmpIsDown = false;
-//     _tmpInputPosition = Vector2.zero;
-// 
-//     constructor() {
-//         super();
-//     }
-// 
-//     start() {
-//         window.addEventListener('touchstart', this._onTouchStart.bind(this));
-//         window.addEventListener('touchmove', this._onTouchMove.bind(this));
-//         window.addEventListener('touchend', this._onTouchEnd.bind(this));
-//     }
-// 
-//     update() {
-//         this.updateInternal({
-//             inputPosition: this._tmpInputPosition,
-//             isDown: this._tmpIsDown,
-//         });
-//     }
-// 
-//     _onTouchStart(e: TouchEvent) {
-//         this._tmpIsDown = true;
-//         const t = e.touches[0];
-//         this.setInputPosition(t.clientX, t.clientY);
-//     }
-// 
-//     _onTouchMove(e: TouchEvent) {
-//         const t = e.touches[0];
-//         this.setInputPosition(t.clientX, t.clientY);
-//     }
-// 
-//     _onTouchEnd(e: TouchEvent) {
-//         this._tmpIsDown = false;
-//         const t = e.touches[0];
-//         this.setInputPosition(t.clientX, t.clientY);
-//         // this.setInputPosition(-Infinity, -Infinity);
-//     }
-// 
-//     setInputPosition(x: number, y: number) {
-//         this._tmpInputPosition.set(x, y);
-//     }
-// 
-//     dispose() {
-//         window.removeEventListener('touchstart', this._onTouchStart.bind(this));
-//         window.removeEventListener('touchmove', this._onTouchMove.bind(this));
-//         window.removeEventListener('touchend', this._onTouchEnd.bind(this));
-//     }
-// }
-
-
 export function createTouchInputController()  {
     let _tmpIsDown = false;
     const _tmpInputPosition = Vector2.zero;
