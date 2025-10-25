1abcb68ec7ac1dcd0a21b6cd42cc8c3f9e6ecf2e
takumifukasawa <takumifukasawa@gmail.com>
2025-10-25 14:57:52 +0900

    func,const

--- src/PaleGL/utilities/mathUtilities.ts
+++ src/PaleGL/utilities/mathUtilities.ts
@@ -31,10 +31,10 @@ import {createVector3, setV3, Vector3} from '@/PaleGL/math/vector3.ts';
  * @param sigma
  * @param x
  */
-export function gaussCoefficient(sigma: number, x: number) {
+export const gaussCoefficient = (sigma: number, x: number) => {
     const sigma2 = sigma * sigma;
     return Math.exp(-(x * x) / (2 * sigma2));
-}
+};
 
 /**
  *
@@ -42,17 +42,13 @@ export function gaussCoefficient(sigma: number, x: number) {
  * @param min
  * @param max
  */
-export function clamp(x: number, min: number, max: number) {
-    return Math.min(max, Math.max(x, min));
-}
+export const clamp = (x: number, min: number, max: number) => Math.min(max, Math.max(x, min));
 
 /**
  *
  * @param x
  */
-export function saturate(x: number) {
-    return clamp(x, 0, 1);
-}
+export const saturate = (x: number) => clamp(x, 0, 1);
 
 /**
  *
@@ -60,51 +56,43 @@ export function saturate(x: number) {
  * @param b
  * @param t
  */
-export function lerp(a: number, b: number, t: number) {
-    return a + (b - a) * t;
-}
+export const lerp = (a: number, b: number, t: number) => a + (b - a) * t;
 
 /**
  *
  * @param a
  * @param b
  */
-export function randomRange(a: number, b: number) {
-    return lerp(a, b, Math.random());
-}
+export const randomRange = (a: number, b: number) => lerp(a, b, Math.random());
 
 /**
  *
  * @param rad
  */
-export function rad2Deg(rad: number) {
-    return rad * (180 / Math.PI);
-}
+export const rad2Deg = (rad: number) => rad * (180 / Math.PI);
 
 /**
  *
  * @param deg
  */
-export function deg2Rad(deg: number) {
-    return deg * (Math.PI / 180);
-}
+export const deg2Rad = (deg: number) => deg * (Math.PI / 180);
 
 // ref: https://gist.github.com/avilde/3736a903560b35fd587d213a3f79fad7
-function mulberry32(seed: number): number {
+const mulberry32 = (seed: number): number => {
     seed |= 0;
     seed = (seed + 0x6d2b79f5) | 0;
     let imul = Math.imul(seed ^ (seed >>> 15), 1 | seed);
     imul = (imul + Math.imul(imul ^ (imul >>> 7), 61 | imul)) ^ imul;
     return ((imul ^ (imul >>> 14)) >>> 0) / 4294967296;
-}
+};
 
-export function generateRandomValue(seed: number, id: number): number {
+export const generateRandomValue = (seed: number, id: number): number => {
     // seedとidを組み合わせて一意の値にする
     const combinedSeed = seed ^ id;
     return mulberry32(combinedSeed);
-}
+};
 
-export function randomOnUnitSphere(seed: number, v: Vector3) {
+export const randomOnUnitSphere = (seed: number, v: Vector3) => {
     const theta = lerp(0, Math.PI * 2, generateRandomValue(seed, 0));
     const phi = Math.asin(generateRandomValue(seed, 1) * 2 - 1);
 
@@ -114,23 +102,21 @@ export function randomOnUnitSphere(seed: number, v: Vector3) {
 
     // return new Vector3(x, y, z);
     setV3(v, x, y, z);
-}
+};
 
-export function randomOnUnitPlane(seed: number, scale: number = 1) {
+export const randomOnUnitPlane = (seed: number, scale: number = 1) => {
     const x = generateRandomValue(seed, 0) * 2 - 1;
     const z = generateRandomValue(seed, 1) * 2 - 1;
     return createVector3(x * scale, 0, z * scale);
-}
+};
 
-export function randomOnUnitCircle(id: number, scale: number, v: Vector3) {
+export const randomOnUnitCircle = (id: number, scale: number, v: Vector3) => {
     const t = lerp(0, Math.PI * 2, generateRandomValue(10, id));
     const x = Math.cos(t);
     const z = Math.sin(t);
     const r = scale * Math.sqrt(generateRandomValue(20, id));
     // return new Vector3(x * r, 0, z * r);
     setV3(v, x * r, 0, z * r);
-}
+};
 
-export function random() {
-    return Math.random();
-}
+export const random = () => Math.random();
