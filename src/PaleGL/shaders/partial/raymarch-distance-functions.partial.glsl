9a79579a802ba2d93cf3dd922b69662262d27c5f
takumifukasawa <takumifukasawa@gmail.com>
2025-11-12 01:34:24 +0900

    wire cube func

--- src/PaleGL/shaders/partial/raymarch-distance-functions.partial.glsl
+++ src/PaleGL/shaders/partial/raymarch-distance-functions.partial.glsl
@@ -144,6 +144,17 @@ float fDfRBoxt(vec3 p, vec3 b, float r)
     return fDfRBox(p, b * .5, r);
 }
 
+// box frame
+float fDfBoxFrame(vec3 p, vec3 b, float e) {
+    p = abs(p)-b;
+    vec3 q = abs(p+e)-e;
+    return min(min(
+        length(max(vec3(p.x,q.y,q.z),0.0))+min(max(p.x,max(q.y,q.z)),0.0),
+        length(max(vec3(q.x,p.y,q.z),0.0))+min(max(q.x,max(p.y,q.z)),0.0)),
+        length(max(vec3(q.x,q.y,p.z),0.0))+min(max(q.x,max(q.y,p.z)),0.0));
+}
+
+
 // capsule
 // a ... capsuleの下の球の中心
 // b ... capsuleの上の球の中心
