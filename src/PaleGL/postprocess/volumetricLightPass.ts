2f1b8977212ed8186a7c7921c1f6f514c388afa1
takumifukasawa <takumifukasawa@gmail.com>
2025-11-16 00:09:54 +0900

    remoe log

--- src/PaleGL/postprocess/volumetricLightPass.ts
+++ src/PaleGL/postprocess/volumetricLightPass.ts
@@ -264,7 +264,6 @@ export function renderVolumetricLightPass(postProcessPass: PostProcessPassBase, 
                 volumetricLightPass.spotLightFrustumMaterial
             );
         }
-        console.log("hogehoge", spotLight.shadowCamera)
     });
 
     setMaterialUniformValue(
