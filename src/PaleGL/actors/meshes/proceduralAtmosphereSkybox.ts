7646b65922a1009103f15303e5180d927f9fd4e4
takumifukasawa <takumifukasawa@gmail.com>
2025-11-15 16:21:00 +0900

    coef skybox

--- src/PaleGL/actors/meshes/proceduralAtmosphereSkybox.ts
+++ src/PaleGL/actors/meshes/proceduralAtmosphereSkybox.ts
@@ -1,26 +1,11 @@
-import { NeedsShorten } from '@/Marionetter/types';
 import { subscribeActorOnStart, subscribeActorOnUpdate } from '@/PaleGL/actors/actor.ts';
 import { createSkybox, Skybox } from '@/PaleGL/actors/meshes/skybox.ts';
-import {
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_SUN_POSITION,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_SUN_INTENSITY,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_PLANET_RADIUS,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_ATMOSPHERE_RADIUS,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_RAYLEIGH_COEFFICIENT,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_MIE_COEFFICIENT,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_RAYLEIGH_SCALE_HEIGHT,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_MIE_SCALE_HEIGHT,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_MIE_SCATTERING_DIRECTION,
-    PROCEDURAL_ATMOSPHERE_SKYBOX_PROPERTY_NAME_CAMERA_ALTITUDE,
-} from './proceduralAtmosphereSkyboxConstants';
 import { UNIFORM_NAME_CUBE_TEXTURE, UNIFORM_TYPE_FLOAT, UNIFORM_TYPE_VECTOR3 } from '@/PaleGL/constants.ts';
-import type { CubeMap } from '@/PaleGL/core/cubeMap.ts';
 import { Gpu } from '@/PaleGL/core/gpu.ts';
 import {
     createProceduralCubeMap,
     CubeMapWithUpdateContext,
     updateProceduralCubeMap,
-    type CubeMapUpdateContext,
 } from '@/PaleGL/core/proceduralCubeMap.ts';
 import { tryStartMaterial } from '@/PaleGL/core/renderer.ts';
 import { setUniformValue, type UniformsData } from '@/PaleGL/core/uniforms.ts';
@@ -72,29 +57,31 @@ const uniformNameMieScaleHeight = 'uMieScaleHeight';
 const uniformNameMieScatteringDirection = 'uMieScatteringDirection';
 const uniformNameCameraAltitude = 'uCameraAltitude';
 
+export const SKYBOX_EPS_SCALE = 0.0001;
+
 export const createProceduralAtmosphereSkybox: (
     args: ProceduralAtmosphereSkyboxOptions
 ) => ProceduralAtmosphereSkybox = (args: ProceduralAtmosphereSkyboxOptions) => {
-   const {
-       gpu,
-       size = 256,
-       updateInterval = 2,
-       baseIntensity = 0.2,
-       specularIntensity = 0.2,
-       rotationOffset = 0,
-       fragmentShader = proceduralCubeMapAtmosphereFragmentShader,
-       sunPosition = [1, 1, 0],
-       sunIntensity = 22.0,
-       planetRadius = 6371e3,
-       atmosphereRadius = 6471e3,
-       rayleighCoefficient = [5.5e-6, 13.0e-6, 22.4e-6],
-       mieCoefficient = 21e-6,
-       rayleighScaleHeight = 8e3,
-       mieScaleHeight = 1.2e3,
-       mieScatteringDirection = 0.758,
-       cameraAltitude = 100.0,
-   } = args; 
-    
+    const {
+        gpu,
+        size = 256,
+        updateInterval = 2,
+        baseIntensity = 0.2,
+        specularIntensity = 0.2,
+        rotationOffset = 0,
+        fragmentShader = proceduralCubeMapAtmosphereFragmentShader,
+        sunPosition = [1, 1, 0],
+        sunIntensity = 22.0,
+        planetRadius = 6371e3,
+        atmosphereRadius = 6471e3,
+        cameraAltitude = 100.0,
+        rayleighCoefficient = [5.5e-2, 13.0e-2, 22.4e-2], // needs eps scale
+        rayleighScaleHeight = 8e3,
+        mieCoefficient = 21e-2, // needs eps scale
+        mieScaleHeight = 1.2e3,
+        mieScatteringDirection = 0.758,
+    } = args;
+
     // let cubeMap: CubeMap;
     // let cubeMapUpdateContext: CubeMapUpdateContext | null = null;
     let frameCount = 0;
@@ -114,14 +101,19 @@ export const createProceduralAtmosphereSkybox: (
         sunIntensity,
         planetRadius,
         atmosphereRadius,
-        rayleighCoefficient: createVector3(rayleighCoefficient[0], rayleighCoefficient[1], rayleighCoefficient[2]),
-        mieCoefficient,
+        cameraAltitude,
+        rayleighCoefficient: createVector3(
+            rayleighCoefficient[0] * SKYBOX_EPS_SCALE,
+            rayleighCoefficient[1] * SKYBOX_EPS_SCALE,
+            rayleighCoefficient[2] * SKYBOX_EPS_SCALE
+        ),
         rayleighScaleHeight,
+        mieCoefficient: mieCoefficient * SKYBOX_EPS_SCALE,
         mieScaleHeight,
         mieScatteringDirection,
-        cameraAltitude,
         cubeMapWithUpdateContext: null,
     };
+    
 
     // onStartでcubemapを生成
     subscribeActorOnStart(skyboxMesh, ({ renderer }) => {
@@ -131,12 +123,12 @@ export const createProceduralAtmosphereSkybox: (
             [uniformNameSunIntensity, UNIFORM_TYPE_FLOAT, skyboxMesh.sunIntensity],
             [uniformNamePlanetRadius, UNIFORM_TYPE_FLOAT, skyboxMesh.planetRadius],
             [uniformNameAtmosphereRadius, UNIFORM_TYPE_FLOAT, skyboxMesh.atmosphereRadius],
+            [uniformNameCameraAltitude, UNIFORM_TYPE_FLOAT, skyboxMesh.cameraAltitude],
             [uniformNameRayleighCoefficient, UNIFORM_TYPE_VECTOR3, skyboxMesh.rayleighCoefficient],
-            [uniformNameMieCoefficient, UNIFORM_TYPE_FLOAT, skyboxMesh.mieCoefficient],
             [uniformNameRayleighScaleHeight, UNIFORM_TYPE_FLOAT, skyboxMesh.rayleighScaleHeight],
+            [uniformNameMieCoefficient, UNIFORM_TYPE_FLOAT, skyboxMesh.mieCoefficient],
             [uniformNameMieScaleHeight, UNIFORM_TYPE_FLOAT, skyboxMesh.mieScaleHeight],
             [uniformNameMieScatteringDirection, UNIFORM_TYPE_FLOAT, skyboxMesh.mieScatteringDirection],
-            [uniformNameCameraAltitude, UNIFORM_TYPE_FLOAT, skyboxMesh.cameraAltitude],
             // ['uSunPosition', UNIFORM_TYPE_VECTOR3, sunPosition],
             // ['uSunIntensity', UNIFORM_TYPE_FLOAT, sunIntensity],
             // ['uPlanetRadius', UNIFORM_TYPE_FLOAT, planetRadius],
@@ -156,7 +148,11 @@ export const createProceduralAtmosphereSkybox: (
         skyboxMesh.cubeMapWithUpdateContext = result;
 
         // Set cubemap uniform
-        setUniformValue(skyboxMesh.materials[0].uniforms, UNIFORM_NAME_CUBE_TEXTURE, skyboxMesh.cubeMapWithUpdateContext.cubeMap);
+        setUniformValue(
+            skyboxMesh.materials[0].uniforms,
+            UNIFORM_NAME_CUBE_TEXTURE,
+            skyboxMesh.cubeMapWithUpdateContext.cubeMap
+        );
 
         tryStartMaterial(gpu, renderer, skyboxMesh.geometry, skyboxMesh.materials[0]);
     });
@@ -164,7 +160,7 @@ export const createProceduralAtmosphereSkybox: (
     const updateParameters = () => {
         if (skyboxMesh.cubeMapWithUpdateContext) {
             const targetMaterialUniforms = skyboxMesh.cubeMapWithUpdateContext.updateContext.material.uniforms!;
-            console.log(targetMaterialUniforms)
+            console.log(targetMaterialUniforms);
             // console.log("======================");
             // console.log(skyboxMesh);
             // console.log(sunPosition);
@@ -181,16 +177,17 @@ export const createProceduralAtmosphereSkybox: (
             setUniformValue(targetMaterialUniforms, uniformNameSunIntensity, skyboxMesh.sunIntensity);
             setUniformValue(targetMaterialUniforms, uniformNamePlanetRadius, skyboxMesh.planetRadius);
             setUniformValue(targetMaterialUniforms, uniformNameAtmosphereRadius, skyboxMesh.atmosphereRadius);
+            setUniformValue(targetMaterialUniforms, uniformNameCameraAltitude, skyboxMesh.cameraAltitude);
             setUniformValue(targetMaterialUniforms, uniformNameRayleighCoefficient, skyboxMesh.rayleighCoefficient);
-            setUniformValue(targetMaterialUniforms, uniformNameMieCoefficient, skyboxMesh.mieCoefficient);
             setUniformValue(targetMaterialUniforms, uniformNameRayleighScaleHeight, skyboxMesh.rayleighScaleHeight);
+            setUniformValue(targetMaterialUniforms, uniformNameMieCoefficient, skyboxMesh.mieCoefficient);
             setUniformValue(targetMaterialUniforms, uniformNameMieScaleHeight, skyboxMesh.mieScaleHeight);
             setUniformValue(
                 targetMaterialUniforms,
                 uniformNameMieScatteringDirection,
                 skyboxMesh.mieScatteringDirection
             );
-            setUniformValue(targetMaterialUniforms, uniformNameCameraAltitude, skyboxMesh.cameraAltitude);
+            // console.log("hogehoge", rayleighCoefficient, mieCoefficient);
         }
     };
 
@@ -199,7 +196,11 @@ export const createProceduralAtmosphereSkybox: (
             frameCount++;
             if (frameCount % skyboxMesh.cubeMapWithUpdateContext.updateContext.updateInterval === 0) {
                 // updateParameters();
-                updateProceduralCubeMap(renderer, skyboxMesh.cubeMapWithUpdateContext.cubeMap, skyboxMesh.cubeMapWithUpdateContext.updateContext);
+                updateProceduralCubeMap(
+                    renderer,
+                    skyboxMesh.cubeMapWithUpdateContext.cubeMap,
+                    skyboxMesh.cubeMapWithUpdateContext.updateContext
+                );
             }
         }
     });
