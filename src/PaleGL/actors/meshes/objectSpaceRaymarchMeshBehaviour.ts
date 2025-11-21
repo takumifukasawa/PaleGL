42b629373b98463d484246caccde9d1d86dadc10
takumifukasawa <takumifukasawa@gmail.com>
2025-11-21 17:50:50 +0900

    remove unused codes

--- src/PaleGL/actors/meshes/objectSpaceRaymarchMeshBehaviour.ts
+++ src/PaleGL/actors/meshes/objectSpaceRaymarchMeshBehaviour.ts
@@ -1,23 +1,17 @@
-import { Actor } from '@/PaleGL/actors/actor.ts';
-import { UpdateActorFunc } from '@/PaleGL/actors/actorBehaviours.ts';
-import { isPerspectiveCamera } from '@/PaleGL/actors/cameras/cameraBehaviours.ts';
-import { setUniformValueToAllMeshMaterials, UpdateMeshMaterial } from '@/PaleGL/actors/meshes/meshBehaviours.ts';
+import { setUniformValueToAllMeshMaterials } from '@/PaleGL/actors/meshes/meshBehaviours.ts';
 import { ObjectSpaceRaymarchMesh } from '@/PaleGL/actors/meshes/objectSpaceRaymarchMesh.ts';
-import { UNIFORM_NAME_OBJECT_SPACE_RAYMARCH_BOUNDS_SCALE } from '@/PaleGL/constants.ts';
-import { setMaterialUniformValue } from '@/PaleGL/materials/material.ts';
-import { updateMaterialSkyboxUniforms } from '@/PaleGL/postprocess/deferredShadingPass.ts';
 
 const UNIFORM_NAME_PERSPECTIVE_FLAG = 'uIsPerspective';
 const UNIFORM_NAME_USE_WORLD_FLAG = 'uUseWorld';
 
 // export const updateObjectSpaceRaymarchMesh: UpdateActorFunc = (actor: Actor) => {
 //     const mesh = actor as ObjectSpaceRaymarchMesh;
-// 
+//
 //     // // for debug
 //     // console.log("============")
 //     // mesh.transform.getScale().log()
 //     // console.log("============")
-// 
+//
 //     mesh.materials.forEach((material) => {
 //         // local
 //         setMaterialUniformValue(material, UNIFORM_NAME_OBJECT_SPACE_RAYMARCH_BOUNDS_SCALE, mesh.transform.scale);
@@ -31,7 +25,7 @@ const UNIFORM_NAME_USE_WORLD_FLAG = 'uUseWorld';
 //         // material.uniforms.setValue(UNIFORM_NAME_OBJECT_SPACE_RAYMARCH_BOUNDS_SCALE, this.transform.getWorldScale());
 //     });
 // };
-// 
+//
 // export const updateObjectSpaceRaymarchMeshMaterial: UpdateMeshMaterial = (mesh, { camera, skybox }) => {
 //     mesh.materials.forEach((material) => {
 //         if (skybox) {
@@ -40,7 +34,7 @@ const UNIFORM_NAME_USE_WORLD_FLAG = 'uUseWorld';
 //         setMaterialUniformValue(material, UNIFORM_NAME_PERSPECTIVE_FLAG, isPerspectiveCamera(camera) ? 1 : 0);
 //     });
 // };
-// 
+//
 // export const updateObjectSpaceRaymarchDepthMaterial: UpdateMeshMaterial = (mesh, { camera }) => {
 //     mesh.depthMaterials.forEach((material) => {
 //         setMaterialUniformValue(material, UNIFORM_NAME_PERSPECTIVE_FLAG, isPerspectiveCamera(camera) ? 1 : 0);
