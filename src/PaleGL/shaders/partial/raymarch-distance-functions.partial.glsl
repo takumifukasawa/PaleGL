df4de1aa63f35e9aba17ff6fbe4898ffef19943c
takumifukasawa <takumifukasawa@gmail.com>
2025-11-23 11:37:07 +0900

    glow pillar

--- src/PaleGL/shaders/partial/raymarch-distance-functions.partial.glsl
+++ src/PaleGL/shaders/partial/raymarch-distance-functions.partial.glsl
@@ -226,6 +226,12 @@ float fDfTorus(vec3 p, vec2 t)
 //     return d * ((q.x * c.y - q.y * c.x < 0.) ? -1. : 1.);
 // }
 
+
+float fDfCappedCylinder(vec3 p, float r, float h) {
+    vec2 d = abs(vec2(length(p.xz),p.y)) - vec2(r,h);
+    return min(max(d.x,d.y),0.0) + length(max(d,0.0));
+}
+
 // ra: 太さ
 // rb: R
 // h: 高さ
